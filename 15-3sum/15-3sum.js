d55ec7f2c4892886e284add1e650c75bfcc1e568
aghildev <85977047+aghildev@users.noreply.github.com>
2022-10-08 11:17:39 +0530

    Time: 260 ms (37.38%), Space: 53.1 MB (37.90%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    let targetSum = 0;
+    let triplets = [];
+    nums.sort((a,b)=>a-b)
+    for(let i = 0;i<nums.length-2;i++){
+        let left = i + 1;
+        let right = nums.length - 1
+        if(nums[i]>0)break;
+        if(i >0 && nums[i]==nums[i-1]  ) continue;
+        while (left < right){
+          let  currentSum = nums[i]+nums[left]+nums[right]
+            if(currentSum == targetSum){
+                triplets.push([nums[i],nums[left],nums[right]])
+                
+                while(nums[left]==nums[left+1]) left++
+                while(nums[right]==nums[right-1])right--
+                left++
+                right--
+            }else if(currentSum < targetSum){
+                     left++
+            }else if(currentSum > targetSum){
+                right--
+            }
+           
+        }
+    }
+    return triplets
+};
