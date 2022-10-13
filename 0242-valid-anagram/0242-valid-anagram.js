1f0cf18e4c3bddbc00e6cd05bf83bf62cdf514f5
aghildev <85977047+aghildev@users.noreply.github.com>
2022-10-13 21:44:04 +0530

    Time: 141 ms (43.22%), Space: 44.6 MB (43.50%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if(s.length != t.length){
+        return false
+    }
+    let obj = {}
+    for(let char of s){
+        obj[char] ? obj[char]+=1:obj[char] = 1
+    }
+    for(let char of t){
+        if(!obj[char]){
+            return false
+        }else{
+            obj[char]-=1
+        }
+    }
+    return true
+};
